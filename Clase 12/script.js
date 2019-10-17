dea3ce6bc14a4a2622202615be50ffb91e193ac5
Jose <romajo_7@hotmail.com>
2019-10-16 21:31:45 -0500

    Primer clase de Java Script

---
+++ Clase 12/script.js
@@ -0,0 +1,21 @@
+function showHello(name) {
+    alert("Hello " + name)
+}    
+function askName(){
+    var name = prompt("Ingresa nombre");
+    alert("Hello " + name);
+}
+
+var nameInput = document.getElementById("name");
+
+nameInput.addEventListener("keyup", function(event){
+    if(event.keyCode === 13) {
+        event.preventDefault();
+        showName();
+    }
+});
+
+function showName(){
+    var name = document.getElementById("name").value;
+    alert("Hello " + name);
+}
